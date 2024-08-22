e1749c1ab9a732f9dede18b16019f2d9ef8165b3
Marius Meyer <marius.meyer@uni-paderborn.de>
2024-08-22 15:32:49 +0200

    Windows support for ocl_icd

--- ocl_icd/build_tarballs.jl
+++ ocl_icd/build_tarballs.jl
@@ -3,14 +3,37 @@ using BinaryBuilder
 name = "ocl_icd"
 version = v"2.3.2"
 sources = [
-    GitSource("https://github.com/OCL-dev/ocl-icd.git", "fdde6677b21329432db8b481e2637cd10f7d3cb2")
+    GitSource("https://github.com/OCL-dev/ocl-icd.git", "fdde6677b21329432db8b481e2637cd10f7d3cb2"),
+    DirectorySource("$(pwd())/patches")
 ]
 
 script = raw"""
 apk add ruby
 
 cd ${WORKSPACE}/srcdir/ocl-icd
+
+# Copy License
+mkdir -p ${WORKSPACE}/destdir/share/licenses/ocl_icd
+cp COPYING ${WORKSPACE}/destdir/share/licenses/ocl_icd
+
+# Fix build for windows based on 
+# https://cygwin.com/cgit/cygwin-packages/ocl-icd/tree/ocl-icd-2.3.2-1.src.patch
+if [[ "${target}" == *-w64-* ]]; then
+    git apply ../cygwin.patch
+fi
+
 ./bootstrap
+
+# Fix rpl_malloc error with musl
+if [[ "${target}" == *-musl* ]]; then
+    sed -i '/AC_FUNC_MALLOC/d' ./configure.ac
+    sed -i '/AC_FUNC_REALLOC/d' ./configure.ac
+fi
+if [[ "${target}" == *-apple-* ]]; then
+    sed -i '/AC_FUNC_MALLOC/d' ./configure.ac
+    sed -i '/AC_FUNC_REALLOC/d' ./configure.ac
+fi
+
 ./configure --prefix=${prefix} \
     --build=${MACHTYPE} \
     --host=${target}
@@ -19,7 +42,7 @@ make install
 """
 
 platforms = supported_platforms()
-filter!(p -> libc(p) == "glibc", platforms)
+filter!(p -> !(Sys.iswindows(p) && arch(p) == "i686"), platforms)
 
 products = [
     LibraryProduct(["libOpenCL", "OpenCL"], :libocl_icd),
@@ -28,7 +51,7 @@ products = [
 
 
 dependencies = [
-    BuildDependency("OpenCL_Headers_jll"),
+    Dependency("dlfcn_win32_jll", platforms=filter(Sys.iswindows, platforms))
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
